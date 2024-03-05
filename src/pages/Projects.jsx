129337f580cb9374fdb954639647b316473fadff
niteshn1 <niteshnegi50@gmail.com>
2024-03-05 16:29:42 +0530

    project Page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CustomTable from "../components/organisms/CustomTable";
 import Button from "../components/atoms/Button";
+import Badge from "../components/atoms/Badge";
 
 const Projects = () => {
   const getHeaderData = () => {
@@ -16,8 +17,15 @@ const Projects = () => {
 
   const tableData = () => {
     return [
-      ["Verra", 12, "New", "Verified", "VERRA", <ActionButtons onViewClick={() => console.log("View clicked")} onActionClick={() => console.log("Action clicked")} />],
-      ["Verra", 92, "New", "Action Required", "GOLD STANDARD", <ActionButtons onViewClick={() => console.log("View clicked")} onActionClick={() => console.log("Action clicked")} />]
+      ["Verra", 12, "New", <Badge varient="">Verified</Badge>,, "VERRA", <ActionButtons />],
+      [
+        "Verra",
+        92,
+        "New",
+        <Badge varient="">Action Required</Badge>,
+        "GOLD STANDARD",
+        <ActionButtons />
+      ]
     ];
   };
 
@@ -47,53 +55,10 @@ const Projects = () => {
     </div>
   );
 
-  const ProjectStatusButton = ({ project }) => {
-    const statusContainer =
-      "rounded-full w-36 h-5 text-xs border-solid mx-auto border-2 pt-2 pb-6  ";
-    const capitalizeContent =
-      typeof project["Status"] === "string"
-        ? project["Status"].charAt(0).toUpperCase() + project["Status"].slice(1)
-        : project["Status"];
-    if (capitalizeContent === "Action Required") {
-      return (
-        <button
-          color={"blue"}
-          varient={"secondary"}
-          className={` ${statusContainer} text-red-600  border-red-600 bg-red-100`}
-        >
-          Action Required
-        </button>
-      );
-    }
-    if (capitalizeContent === "Verified") {
-      return (
-        <button
-          color={"blue"}
-          varient={"secondary"}
-          className={` ${statusContainer} capitalize text-green-600 border-green-600 bg-green-100`}
-        >
-          Verified
-        </button>
-      );
-    }
-
-    if (capitalizeContent === "Verification") {
-      return (
-        <button
-          color={"blue"}
-          varient={"secondary"}
-          className={` ${statusContainer} capitalize text-teal-500 border-teal-500 bg-teal-100`}
-        >
-          Verification
-        </button>
-      );
-    }
-    return null;
-  };
-
+ 
   return (
-    <div className="flex  pl-4 pt-4 w-full bg-white border border-green-800">
-      <div className="ml-[14rem] w-[80%] border border-gray-500">
+    <div className="flex   pt-4 w-full bg-white border">
+      <div className="w-full">
         {" "}
         <CustomTable headerData={getHeaderData()} data={tableData()} />
       </div>
