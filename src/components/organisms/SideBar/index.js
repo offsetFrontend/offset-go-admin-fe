d020b838aba6ec198ed16e5e53978bbe7dc697b2
niteshn1 <niteshnegi50@gmail.com>
2024-03-05 16:20:19 +0530

    pascalcase

--- src/components/organisms/SideBar/index.js
+++ src/components/organisms/SideBar/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { SideBarLabels } from '../../../utils/labels/sidebarLabels'
-
 import NavButton from './navButton'
 
 const SideBar = () => {
