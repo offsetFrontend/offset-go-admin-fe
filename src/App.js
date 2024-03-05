844e1900b5ac15055146053a6a268a92d96b98b8
souravmsi <hudasourav@gmail.com>
2024-03-05 11:34:25 +0530

    commentted code is removed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from "./pages/Login";
 function App() {
   return (
     <main className="font-spaceGrotesk bg-[#F4F6FA] min-h-screen flex max-w-screen-2xl mx-auto">
-      {/* <div className="w-60 h-screen bg-red-100"/> */}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
