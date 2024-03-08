fc26e6ee7cdb812bbbce888200b1584fe2acd0cb
niteshn1 <niteshnegi50@gmail.com>
2024-03-08 17:04:55 +0530

    searchbox implemented

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "../components/atoms/Button";
 import Badge from "../components/atoms/Badge";
 import { ReactComponent as Filter } from "../assets/svgs/filter.svg";
+import SearchBox from "../components/atoms/SearchBox";
 import Table from "../components/atoms/Table";
 const Transactions = () => {
+  const searchRef = useRef();
   const getHeaderData = () => {
     return ["Transaction ID", "Type", "Amount", "Status", "Date"];
   };
@@ -63,6 +65,9 @@ const Transactions = () => {
   const handleStatusButton = () => {
     console.log("status button clicked");
   };
+  const handleSearch = () => {
+    console.log(searchRef.current.value);
+  };
 
   return (
     <div className="flex  flex-col pt-4 w-full bg-gray-100 border">
@@ -70,14 +75,15 @@ const Transactions = () => {
 
       <div className="w-full h-full pt-4 mt-8 bg-white rounded-3xl">
         <div className="flex justify-between pt-5 pb-12">
-          <div className="w-80 border-2 border-grey-800 rounded-lg mx-5 mb-5 "></div>
-
+          <div className="flex justify-between py-5 px-8">
+            <SearchBox ref={searchRef} onSearch={handleSearch} />
+          </div>
           <div className="mr-10 ">
             <Button
               className="flex justify-center items-center gap-x-2 px-2  py-2 h-fit"
               borderColor={"gray"}
               varient={"secondary"}
-              // onClick={handleFilterButtonClick}
+           
             >
               <Filter className="w-4 h-4" />
               <span>Filter</span>
