3ba7ca2d3e65670ecea219f5d79a09e9f3c71509
niteshn1 <niteshnegi50@gmail.com>
2024-03-12 12:26:50 +0530

    quickfix

--- src/components/atoms/Badge/index.jsx
+++ src/components/atoms/Badge/index.jsx
@@ -26,16 +26,13 @@ const background_style = {
   verification: "bg-teal-500  bg-opacity-15 rounded-full",
   "action required": "bg-red-500  bg-opacity-15 rounded-full"
 };
-const text_style = {
-  green: "text-teal-500",
-  red: "text-red-600"
-};
+
 const Badge = ({ children, varient }) => {
   return (
     <div
-      className={`w-fit ${border_style[varient.toLowerCase()]} ${
-        text_style[varient.toLowerCase()]
-      }`}
+    className={`w-fit ${
+      border_style[varient.toLowerCase()]
+    }`}
     >
       <div className={` capitalize ${background_style[varient.toLowerCase()]}`}>
         {children}
